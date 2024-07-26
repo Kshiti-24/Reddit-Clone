5a681ff432bcc370424a66bd7fae27d191978be1
Kshitiz <kshitizagarwa2405@gmail.com>
2024-07-26 14:21:08 +0530

    Initial Commit

---
+++ lib/Core/Common/sign_in_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit/Core/Constants/constants.dart';
+import 'package:reddit/Features/Auth/Controller/auth_controller.dart';
+import 'package:reddit/Themes/themes.dart';
+
+class SignInButton extends ConsumerWidget {
+  final bool isFromLogin;
+  const SignInButton({Key? key, this.isFromLogin = true}) : super(key: key);
+
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithGoogle();
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.all(18.0),
+      child: ElevatedButton.icon(
+        onPressed: () => signInWithGoogle(ref),
+        icon: Image.asset(
+          Constants.googlePath,
+          width: 35,
+        ),
+        label: const Text(
+          'Continue with Google',
+          style: TextStyle(fontSize: 18),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppTheme.greyColor,
+          minimumSize: const Size(double.infinity, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit/Features/Auth/Screens/login_screen.dart';
+import 'package:reddit/Themes/themes.dart';
+import 'package:reddit/firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Reddit App',
+      theme: AppTheme.darkModeAppTheme,
+      home: LoginScreen(),
+    );
+  }
+}
